f5895112af920e5b8c2e22c39e4d8fcf7a33a606
lathanhmtad <d0763705638@gmail.com>
2024-06-11 19:18:30 +0700

    commit

---
+++ src/main/java/exercise/ex4/Question1.java
@@ -0,0 +1,11 @@
+package exercise.ex4;
+
+public class Question1 {
+    public static void main(String[] args) {
+        /*
+            Nhập một xâu kí tự, đếm số lượng các từ trong xâu kí tự đó
+            (các từ có thể cách nhau bằng nhiều khoảng trắng );
+         */
+
+    }
+}
---
+++ src/main/java/exercise/ex4/Question2.java
@@ -0,0 +1,18 @@
+package exercise.ex4;
+
+import java.util.Scanner;
+
+public class Question2 {
+    public static void main(String[] args) {
+        // Nhập hai xâu kí tự s1, s2 nối xâu kí tự s2 vào sau xâu s1
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhập chuỗi s1: ");
+        String s1 = scanner.nextLine();
+
+        System.out.println("Nhập chuỗi s2: ");
+        String s2 = scanner.nextLine();
+
+        System.out.println("s2 + s1 = " + s2.concat(s1));
+    }
+}
