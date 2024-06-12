e24e94bf9702f473c8e50732d8b2df52fbedb6d2
lathanhmtad <d0763705638@gmail.com>
2024-06-12 09:53:01 +0700

    assignment 01

--- src/main/java/exercise/Account.java
+++ src/main/java/exercise/Account.java
@@ -1,9 +1,13 @@
 package exercise;
 
+import java.time.LocalDate;
+
 public class Account {
     int id;
     String email;
     String username;
     String fullName;
     Department department;
+    Position position;
+    LocalDate createdDate;
 }
