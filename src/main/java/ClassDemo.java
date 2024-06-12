ece255775ac0ac2a17914e55e302e27868903f62
lathanhmtad <d0763705638@gmail.com>
2024-06-12 15:17:06 +0700

    lesson 01

--- src/main/java/ClassDemo.java
+++ src/main/java/ClassDemo.java
@@ -5,6 +5,9 @@ public class ClassDemo {
         dog.age = 3;
         dog.bark();
 
+        Dog dog2 = new Dog();
+        System.out.println("dog2.name = " + dog2.name);
+
         System.out.println("dog.name = " + dog.name);
         System.out.println("dog.age = " + dog.age);
 
--- src/main/java/DataTypeDemo.java
+++ src/main/java/DataTypeDemo.java
@@ -1,4 +1,7 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 
 public class DataTypeDemo {
     public static void main(String[] args) {
@@ -9,8 +12,9 @@ public class DataTypeDemo {
 
         // Kiểu số thực
         // float (4 byte), double (8 byte)
-        double pi = 3.1111111111111111555555555d;
-        System.out.println("pi = " + pi);
+        float pif = 3.141555555555F;
+        System.out.println("pif = " + pif);
+        double pi = 3.1111111111111111555555555D;
         System.out.println("pi = " + pi);
 
         long a = 5;
@@ -29,34 +33,54 @@ public class DataTypeDemo {
         // Kiểu sâu ký tự
         // String
         String s = "Java core";
+        System.out.println("s = " + s);
 
         // Kiểu thời gian
-        // LocalDate, LocalTime, LocalDateTime
+        // LocalDate, LocalTime, LocalDateTime, Date
+        Date d = new Date();
+        System.out.println("date = " + d);
+
         LocalDate today = LocalDate.now();
         System.out.println("today = " + today);
 
-        LocalDate date = LocalDate.of(2019, 3, 19);
+        LocalTime lt = LocalTime.now();
+        System.out.println("lt = " + lt);
+
+        LocalDate date = LocalDate.of(2024, 2, 29);
         System.out.println("date = " + date);
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println("localDateTime = " + localDateTime);
 
         // Kiểu enum
         UIState state = UIState.LOADING;
+        UIState state2 = UIState.ERROR;
+        System.out.println("state2 = " + state2);
         System.out.println("state = " + state);
 
         // Kiểu array (mảng)
-        int[] numbers = {1,2,3,4,545};
+        int[] numbers = {1, 2, 3, 4, 545};
         System.out.println("numbers = " + numbers);
         System.out.println("numbers.length = " + numbers.length);
-        // Chỉ số: bắt đầu từ 0
-        System.out.println(numbers[2]);
+        // Chỉ số mảng: bắt đầu từ 0
+        System.out.println("numbers[2] = " + numbers[2]);
 
-        String[] fruits = new String[] {"Cam", "Mận", "Táo"};
+        String[] f;
+        f = new String[]{"Cam", "Quyrt"};
+        System.out.println("f.length = " + f.length);
+        String[] fruits = new String[]{"Cam", "Mận", "Táo"};
         String[] fruits2;
         fruits2 = new String[]{"abc", "xyz"};
         System.out.println("fruits2 = " + fruits2);
 
-        char[] characters = new char[10];
+        char[] chars = new char[10];
+//        char[] chars2 = new char[-5];
+//        System.out.println("chars2.length = " + chars2.length); // java.lang.NegativeArraySizeException
 
-        characters[0] = 'K';
-        System.out.println("characters.length = " + characters.length);
+        // Cập nhập giá trị theo chỉ số
+        chars[0] = 'K';
+        System.out.println("chars[0] = " + chars[0]);
+        System.out.println("chars[1] = " + chars[1]);
+        System.out.println("characters.length = " + chars.length);
     }
 }
