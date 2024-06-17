b540237c8cd9fe4e32d303cf213e288923c4cc81
lathanhmtad <d0763705638@gmail.com>
2024-06-17 16:00:22 +0700

    lesson 04

--- src/main/java/ConstructorDemo.java
+++ src/main/java/ConstructorDemo.java
@@ -3,7 +3,10 @@ public class ConstructorDemo {
         // Constructor: Hàm khởi tạo
         // 1. Tên constructor trùng với tên của class
         // 2. Constructor không có kiểu trả về
-        Dog dog = new Dog();
+        Dog dog = new Dog("Long", 19);
+        System.out.println("dog.toString() = " + dog.toString());
 
+        Dog dog2 = new Dog("Duy");
+        System.out.println("dog2.toString() = " + dog2.toString());
     }
 }
