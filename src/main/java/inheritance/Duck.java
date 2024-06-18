7a5134b8e9338e0c225a1fb219ed2d02fa008d52
lathanhmtad <d0763705638@gmail.com>
2024-06-18 10:28:59 +0700

    lession 04'

--- src/main/java/inheritance/Duck.java
+++ src/main/java/inheritance/Duck.java
@@ -2,4 +2,25 @@ package inheritance;
 
 public class Duck extends Animal {
 
+    Duck() {
+    }
+
+    public Duck(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void eat() {
+
+        super.eat();
+        System.out.println("Duck is eating!");
+    }
+
+    @Override
+    public String toString() {
+        return "Duck{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
