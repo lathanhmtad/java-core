566bc10688eecec2b1dc269a330b28f6896b8b3c
lathanhmtad <d0763705638@gmail.com>
2024-06-26 17:08:56 +0700

    assignment lesson 03

---
+++ src/main/java/exercise/extra/Question5.java
@@ -0,0 +1,15 @@
+package exercise.extra;
+
+public class Question5 {
+    public static void main(String[] args) {
+        /*
+        Question 5:
+        Nhập xâu. Thống kê số các chữ số ‘0’, số chữ số ‘1’, …, số chữ số ‘9’
+        trong xâu
+        */
+        String s = "DU11230Y982NG51UYEN00DANGNA123";
+        int length = s.length();
+        // char[]
+
+    }
+}
