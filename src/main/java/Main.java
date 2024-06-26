c08d79f896ec4730208670d466db493f4b202a94
lathanhmtad <d0763705638@gmail.com>
2024-06-26 22:47:22 +0700

    lesson 09

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        Integer a = 100;
-        Integer b = 100;
-        System.out.println(a == b);
+//        Integer a = 100;
+//        Integer b = 100;
+//        System.out.println(a == b);
+//
+//        Integer c = 200;
+//        Integer d = 200;
+//        System.out.println(c == d);
+//        String.valueOf("Duy");
 
-        Integer c = 200;
-        Integer d = 200;
-        System.out.println(c == d);
-        String.valueOf("Duy");
+
+        // Sai số
+//         double a = 3.25;
+//         double b = 2.2; // 0.2 không là lũy thừa của 2
+//         float d = 2.2F; // a - b == a - d => false
+
+        double a = 3.2;
+        double b = 2.25; // 0.25 là 2^-2
+        float d = 2.25F; // a - b == a - d => true
+
+        // System.out.println(a - b == a - d);
     }
 }
