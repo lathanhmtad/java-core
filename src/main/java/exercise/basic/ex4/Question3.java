d1077f58f5ea3ea4f127ac9f90ce8bea1c4c567d
lathanhmtad <d0763705638@gmail.com>
2024-06-27 19:41:21 +0700

    assignment lesson 02

--- src/main/java/exercise/basic/ex4/Question3.java
+++ src/main/java/exercise/basic/ex4/Question3.java
@@ -6,7 +6,7 @@ public class Question3 {
     public static void main(String[] args) {
         /* Khai báo 1 array bao gồm các tên của các bạn trong lớp,
         sau đó in ngẫu nhiên ra tên của 1 bạn */
-        String names[] = {"Duy", "Vinh", "Hoa", "Du"};
+        String[] names = {"Duy", "Vinh", "Hoa", "Du"};
 
         Random random = new Random();
         System.out.println(names[random.nextInt(names.length)]);
